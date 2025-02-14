043239daf0fcfea774c45e09483860b87fed0f56
Justin Rasink <justin@mac.local>
2025-02-14 17:29:19 +0100

    second initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import Koa from 'koa';
 import { RateMachine } from './lib/rate-limit';
 
+const config = {
+  port: 80,
+  rate: 10,
+  interval: 1000
+};
+
 const getId = (() => { let i = 0; return () => i++; })();
 const getTime = (() => { const t0 = new Date().getTime(); return () => `${(new Date().getTime() - t0) / 1000}s`; })();
 
-const limiter = new RateMachine(3, 1000);
+const limiter = new RateMachine(config.rate, config.interval);
 
 const respond = async (ctx: any) => limiter.execute(async () => {
   ctx.status = 200;
@@ -16,13 +22,13 @@ const app = new Koa();
 app.use(async (ctx) => {
   const id = getId();
 
-  console.log(`${getTime()} >> Incoming request ${id} (${limiter.state().requests} requests are currently queued)`);
+  console.log(`${getTime()} >> Incoming request ${id} (already queued: ${limiter.state().requests})`);
 
   await respond(ctx);
 
-  console.log(`${getTime()} >> Response sent for ${id} (${limiter.state().requests} requests are currently queued)`);
+  console.log(`${getTime()} >> Response sent for request ${id} (still queued: ${limiter.state().requests})`);
 });
 
-app.listen(80);
+app.listen(config.port);
 
-console.log(`${getTime()} >> Listening on port 80`);
+console.log(`${getTime()} >> Listening on port ${config.port}. Will send ${config.rate} responses per ${config.interval}ms.`);
--- test.sh
+++ test.sh
@@ -1,12 +1,27 @@
 #!/bin/sh
 
-# this will send 10 requests sequentially, waiting 100ms in between
-# simulate multiple consumers by running this script multiple times in parallel
-
+nreqs=100
+pids=""
 t0=$(date +%s)
-for run in {1..10}; do
-  sleep .1
-  echo "request $((run)) at t = $(($(date +%s)-$t0))s"
+
+run()
+{
   curl -s -o /dev/null localhost:1337
-  echo "response $((run)) at t = $(($(date +%s)-$t0))s"
+  echo "Got response $(($1)) at t = $(($(date +%s)-$t0))s"
+}
+
+echo "Sending $((nreqs)) requests..."
+
+for i in $(seq 0 $nreqs); do
+  run i &
+  p=$!
+  pids="${pids} ${p}"
 done
+
+echo "All requests underway, now we wait.\n---"
+
+for p in $pids; do
+  wait $p
+done
+
+echo "---\nAll responses received, cheers."
